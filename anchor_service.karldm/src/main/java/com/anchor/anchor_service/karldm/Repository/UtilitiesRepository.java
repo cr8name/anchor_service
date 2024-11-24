86897b935f47835056d439a6f63b16d191c76124
cr8name <75536006+cr8name@users.noreply.github.com>
2024-11-25 03:39:46 +0800

    Delete anchor_service.karldm directory

--- anchor_service.karldm/src/main/java/com/anchor/anchor_service/karldm/Repository/UtilitiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anchor.anchor_service.karldm.Repository;
-
-import com.anchor.anchor_service.karldm.Entity.Utilities;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UtilitiesRepository extends JpaRepository<Utilities,Integer> {
-}
