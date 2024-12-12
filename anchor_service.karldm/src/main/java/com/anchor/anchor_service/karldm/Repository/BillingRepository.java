a260332607d38ca191d5b3edfa305912ad58dcef
cr8name <75536006+cr8name@users.noreply.github.com>
2024-12-13 05:15:55 +0800

    Delete anchor_service.karldm directory

--- anchor_service.karldm/src/main/java/com/anchor/anchor_service/karldm/Repository/BillingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anchor.anchor_service.karldm.Repository;
-
-import com.anchor.anchor_service.karldm.Entity.Billing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billing,Integer> {
-}
